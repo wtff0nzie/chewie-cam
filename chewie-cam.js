40e11b90c319139f02132b9fcf46d9d3b4c6ab21
Dez <dez@backpagecms.net>
2018-05-20 15:12:36 +0100

    Add watcher and early file uploader.

--- chewie-cam.js
+++ chewie-cam.js
@@ -1,15 +1,39 @@
+'use strict';
+
 var shell = require('shelljs'),
+    watch = require('node-watch'),
+    request = require('request'),
+    fs = require('fs'),
+    filePath = './www/fecklet.jpg',
     counter = 0;
 
 
-const takePic = () => {
-    counter++;
-    console.log('Cliicckkkkkk...');
+// Watch for file changes --------------------------
+watch(filePath, {recursive: true}, (evt, name) => {
+    console.log('Watcher', evt, name);
+    uploadPic();
+});
 
-    shell.exec('sudo raspistill -t 2000 -o /home/pi/work/chewie-cam/www/fecklet.jpg');
+
+// Upload a pic ------------------------------------
+const uploadPic = () => {
+    let fileStream = fs.createReadStream(filePath);
+
+    fileStream.on('error',  (err) => {
+        console.error('UploadPic error', err);
+    });
+
+    fileStream.pipe(request.put('https://chewie-cam-dev.herokuapp.com/update'));
 };
 
 
-if (!process.env.server) {
+// Take a pic --------------------------------------
+const takePic = () => {
+    counter++;
+    shell.exec('sudo raspistill -t 1000 -o /home/pi/work/chewie-cam/www/fecklet.jpg');
+};
+
+
+if (!process.env.server || !process.env.dev) {
     setInterval(takePic, 30 * 1000);
 }
